aedd82a178ba10fae672a1b4fec16b94cbab4acf
Emanuele Eichholz <emanueleeichholz@gmail.com>
2023-02-07 19:52:57 -0300

    adjust AlertViewController

--- marvel-app/DesignSystem/Components/AlertViewController/AlertViewController.swift
+++ marvel-app/DesignSystem/Components/AlertViewController/AlertViewController.swift
@@ -11,31 +11,37 @@ extension UIViewController {
     
     func showAlert(model: AlertModel, firstButtonAction: (() -> Void)?, secondButtonAction: (() -> Void)?) {
         
-        let alert = UIAlertController(
-            title: model.title,
-            message: model.message,
-            preferredStyle: .alert
-        )
-        
-        alert.addAction(
-            UIAlertAction(
-                title: model.firstButtonTitle,
-                style: UIAlertAction.Style.default,
-                handler: { action in
-            firstButtonAction?()
-        }))
-                        
-        alert.addAction(
-            UIAlertAction(
-                title: model.secondButtonTitle,
-                style: UIAlertAction.Style.default,
-                handler: { action in
-            secondButtonAction?()
-        }))
-
         DispatchQueue.main.async {
-            self.present(alert, animated: true, completion: nil)
+            
+            let alert = UIAlertController(
+                title: model.title,
+                message: model.message,
+                preferredStyle: .alert
+            )
+            
+            alert.addAction(
+                UIAlertAction(
+                    title: model.firstButtonTitle,
+                    style: UIAlertAction.Style.default,
+                    handler: { action in
+                        firstButtonAction?()
+                    }))
+            
+            alert.addAction(
+                UIAlertAction(
+                    title: model.secondButtonTitle,
+                    style: UIAlertAction.Style.default,
+                    handler: { action in
+                        secondButtonAction?()
+                    }))
+            
+            
+            if !alert.isBeingPresented {
+                self.present(alert, animated: true, completion: nil)
+            }
+            
         }
         
     }
+    
 }
