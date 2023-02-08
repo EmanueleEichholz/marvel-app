03e1d67ef3a4a3c30fa1410316ab13dc7ef38f14
Emanuele Eichholz <emanueleeichholz@gmail.com>
2023-02-07 21:03:08 -0300

    filtered items to display in home screen

--- marvel-app/Scenes/Home/Presenter/HomePresenter.swift
+++ marvel-app/Scenes/Home/Presenter/HomePresenter.swift
@@ -22,90 +22,96 @@ final class HomePresenter: HomePresenterProtocol {
     
     func presentCharacters(with model: CharactersData?) {
         if let model = model {
-            var characterList: [ItemCardModel] = []
-            model.results.forEach { character in
-                characterList.append(
-                    ItemCardModel(
+            var characterList: [ItemCardModel] = model.results.map { character in
+                return ItemCardModel(
                         name: character.name ?? "Name Unavailable",
                         image: getImageURL(
                             path: character.thumbnail?.path,
                             pathExtension: character.thumbnail?.thumbnailExtension
                         )
                     )
-                )
             }
+            
+            characterList = characterList.filter {
+                $0.image != "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/standard_xlarge.jpg"
+            }
+            
             view?.updateCharactersSection(with: characterList)
         }
     }
     
     func presentComics(with model: ComicsData?) {
         if let model = model {
-            var comicsList: [ItemCardModel] = []
-            model.results.forEach { comic in
-                comicsList.append(
-                    ItemCardModel(
+            var comicsList: [ItemCardModel] = model.results.map { comic in
+                return ItemCardModel(
                         name: comic.title ?? "Title Unavailable",
                         image: getImageURL(
                             path: comic.thumbnail?.path,
                             pathExtension: comic.thumbnail?.thumbnailExtension
                         )
                     )
-                )
             }
+            
+            comicsList = comicsList.filter {
+                $0.image != "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/standard_xlarge.jpg"
+            }
+            
             view?.updateComicsSection(with: comicsList)
         }
     }
     
     func presentCreators(with model: CreatorsData?) {
         if let model = model {
-            var creatorsList: [ItemCardModel] = []
-            model.results.forEach { creator in
-                creatorsList.append(
-                    ItemCardModel(
+           let creatorsList: [ItemCardModel] = model.results.map { creator in
+                return ItemCardModel(
                         name: creator.fullName ?? "Name Unavailable",
                         image: getImageURL(
                             path: creator.thumbnail?.path,
                             pathExtension: creator.thumbnail?.thumbnailExtension
                         )
                     )
-                )
             }
+            
             view?.updateCreatorsSection(with: creatorsList)
         }
     }
     
     func presentEvents(with model: EventsData?) {
         if let model = model {
-            var eventsList: [ItemCardModel] = []
-            model.results.forEach { event in
-                eventsList.append(
-                    ItemCardModel(
+            var eventsList: [ItemCardModel] = model.results.map { event in
+                return ItemCardModel(
                         name: event.title ?? "Title Unavailable",
                         image: getImageURL(
                             path: event.thumbnail?.path,
                             pathExtension: event.thumbnail?.thumbnailExtension
                         )
                     )
-                )
             }
+            
+            eventsList = eventsList.filter {
+                $0.image != "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/standard_xlarge.jpg"
+            }
+            
             view?.updateEventsSection(with: eventsList)
         }
     }
     
     func presentSeries(with model: SeriesData?) {
         if let model = model {
-            var seriesList: [ItemCardModel] = []
-            model.results.forEach { serie in
-                seriesList.append(
-                    ItemCardModel(
+            var seriesList: [ItemCardModel] = model.results.map { serie in
+                return ItemCardModel(
                         name: serie.title ?? "Title Unavailable",
                         image: getImageURL(
                             path: serie.thumbnail?.path,
                             pathExtension: serie.thumbnail?.thumbnailExtension
                         )
                     )
-                )
             }
+            
+            seriesList = seriesList.filter {
+                $0.image != "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/standard_xlarge.jpg"
+            }
+            
             view?.updateSeriesSection(with: seriesList)
         }
     }
